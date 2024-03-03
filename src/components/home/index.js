f74fc63d0e561d489673c086e1b6abab274eb364
syed-jaid <syed.jaid.7748@gmail.com>
2024-03-03 12:47:57 +0600

    first commit

---
+++ src/components/home/index.js
@@ -0,0 +1,190 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <div className='bg-black text-white font-bold text-[25px] text-center py-3'>🎉 GET 90% OFF TODAY FOR MARCH!
+                <br />
+                <span className='text-[#39ff14]'> + MORE BONUSES AT CHECKOUT!🎁</span>
+            </div>
+
+
+            <div className='bg-[#e73904] mb-[50px]'>
+                <div className='max-w-[1130px] mx-auto text-center text-white py-4'>
+                    <h1 className='font-bold text-[36px] '>Here's everything you need to...</h1>
+
+                    <h1 className='text-[56px] py-3'>Instantly Unlock Latest 16,000+ Prompts To Train ChatGPT Be Your Personal Digital Intern That Semi-Passively Adds 15-30 Buyers A Day To Your List...
+                    </h1>
+
+                    <h1 className='text-[28px]'>Without Ever Spending Hours Trying To Crack Your Offer & Create Contents...</h1>
+                    <img className='mx-w-[1160px] mx-auto' src="https://images.leadconnectorhq.com/image/f_webp/q_80/r_1200/u_https://assets.cdn.filesafe.space/3cfYZ62ndd3Fci44OcL9/media/658415d73489e429f466089c.png" alt="" />
+
+                    <h1 className='text-[23px] font-bold italic'> **YOU CAN NOT FIND THESE ON CHATGPT!</h1>
+
+                    <div className='max-w-[836px] rounded-full mx-auto text-center text-white bg-[#333333] py-2 mt-3 mb-2'>
+                        <h1 className='text-[30px] font-bold'>
+                            Get All Prompts + $1600 in Bonuses today!
+                        </h1>
+
+                        <h2 className='text-[15px]'>
+                            NO QUESTION ASKED MONEY BACK GUARANTEE
+                        </h2>
+                    </div>
+
+                </div>
+            </div>
+
+
+            <div className=' shadow-lg border max-w-[819px] mx-auto text-center rounded-lg py-4 my-5'>
+                <h1 className='text-[100px]'>🔥</h1>
+                <h1 className='text-[24px] font-bold text-[#e73904]'>Limited Time Sale – 90% OFF</h1>
+                <h1 className='text-[36px] font-bold uppercase max-w-[644px] mx-auto '>GET The Best "Done-For-You" AI ChatGPT PROMPTs + Toolkits for Business & experts on the Internet!</h1>
+                <img className='mx-w-[819px] mx-auto ' src="https://assets.cdn.filesafe.space/3cfYZ62ndd3Fci44OcL9/media/65965c5c539b6d682bbea643.gif" alt="" />
+
+                <div className=' max-w-[650px] mx-auto text-left'>
+                    <ul >
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'> 1000+ Sales Funnel Prompts (including copywriting)</li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'>Facebook™ Ad Prompts</li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'> Instagram™ Ad Prompts</li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'>TikTok™ Ad Prompts</li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'>YouTube™ Ad Prompts</li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'>2000+ Email Marketing Prompts</li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'>Ecommerce Prompts</li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'>Lead Generation Prompts</li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'>Affiliate Marketing Prompts</li>
+                        </div>
+
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'>1400+ Social Media Marketing Prompts</li>
+                        </div>
+
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'> Business Prompts  </li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'> Influencer Marketing Prompts  </li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'> Twitter™ Prompts  </li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'>  LinkedIn™ Prompts </li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'> SEO Prompts  </li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'>  Outreach Message Prompts </li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'> 3600+ Closing Sales Prompts (including closing script)  </li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'>  1000+ Landing Page Prompts </li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'>  1000+ Conversion Rate Optimization Prompts </li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'>  2200+ Productivity Prompts (you'll save time on using proven ChatGPT prompts) </li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'> 950+ Customer Support Prompts  </li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'> 1300+ Product Management Prompts for your most repeated task  </li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'>  1000+ Human Resources Prompts </li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'>   Proven tricks to unlock ChatGPT fully </li>
+                        </div>
+                        <div className='flex py-2'>
+                            <span className='text-[#e93d3d] text-[30px] mr-2 font-bold'> ✓ </span>
+                            <li className='text-[30px] font-bold'> Exact steps + examples to get reliable results & scale  </li>
+                        </div>
+
+                    </ul>
+
+                </div>
+
+                <div className=' max-w-[650px] mx-auto  my-5 '>
+                    <h1 className='text-[24px] font-bold text-[#e73904] border-b-2 pb-5 mb-5'>
+                        Master ChatGPT while your competitors are still trying to figure it out!
+                    </h1>
+                    <div>
+                        <h1 className='text-[24px]'>Total Value $3,997</h1>
+                        <h1 className='line-through text-[24px]'>Retail Value $97</h1>
+                    </div>
+
+                    <h1 className='text-[48px] font-bold '>Get Everything For Just:</h1>
+                    <h1 className='text-[36px] font-bold text-[#e73904] '>$27!</h1>
+
+                    <div className='bg-[#e73904] py-2 rounded-[29px]'>
+                        <h1 className=' text-[28px] font-bold text-white '>GET All Prompts + $1600 in Bonuses <br /> Today!</h1>
+                        <h1 className=' text-[14px]  text-white'> No questions asked money back guarantee </h1>
+                    </div>
+                    <div className=' text-[14px] '>If you curious to know how to consistently get paid to get new clients, here's the secret...</div>
+
+
+                </div>
+
+
+                <div className=' max-w-[650px] mx-auto mt-[60px]'>
+                    <h1 className='text-[24px] text-[#e73904] font-bold'>HERE'S A SHOCKING TRUTH...</h1>
+                    <h1>😱 20 hours</h1>
+                    <h1>👆  That’s how much time per week an average online business or experts can save per week by utilizing AI & ChatGPT to its full potential… 🎉</h1>
+                </div>
+
+
+            </div>
+
+
+
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/thankYou/index.js
@@ -0,0 +1,92 @@
+import React from 'react';
+
+const ThankYouPage = () => {
+    return (
+
+        <div>
+            <div className='bg-[#e73904] flex items-center  justify-center h-[120px]'>
+                <h1 className='text-center font-bold text-[50px] text-white'>Congratulations - order Complete!</h1>
+            </div>
+            <p className=' flex items-center  justify-center text-3xl pt-5'>
+                Kindly <p className='font-bold ml-3' > check your email</p>! You should receive it within 2-5 minutes!
+            </p>
+            <div className=' flex items-center  justify-center text-3xl pt-3'>- OR -</div>
+            <div className=' flex items-center  justify-center text-3xl pt-3'> You can <span className='font-bold text-[#188bf6] ml-2 mr-2 hover:underline'>click here</span> to directly access your <span className='font-bold text-[#188bf6] ml-2 mr-2 hover:underline'>Bonuses </span></div>
+
+
+            <div className="my-5 max-w-[1160px] mx-auto">
+                <iframe
+                    width="1140"
+                    height="606"
+                    src="https://www.youtube.com/embed/dQw4w9WgXcQ" // Use your video's embed link
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                ></iframe>
+            </div>
+
+            <div className='max-w-[1160px] mx-auto text-3xl pt-3 text-center'>
+                If you've any more questions, just drop us an email at <span className='text-[#188bf6] ml-2 mr-2 hover:underline '>support@aitoolkitsforcoaches.com </span> and we will take care of you.
+            </div>
+            <div className='max-w-[1160px] mx-auto text-3xl pt-3 text-center my-[20px]'>
+                If you want to build your funnels and get more clients online, you can <span className='font-bold text-[#188bf6] ml-2 mr-2 hover:underline'>Click The Button Below</span> to schedule a <span className='underline' >FREE Consultation Call</span> with our team👇
+            </div>
+
+            <div className='flex items-center justify-center py-2'>
+                <div className='bg-[#e73904] w-[732px] h-[111] rounded-3xl p-3 text-white flex items-center  justify-center'>
+                    <h1 className='  font-bold text-[30px] '>BOOK A FREE CONSULTATION CALL <br />
+                        <p className='font-normal text-[20px] text-center'> Schedule call with us</p>
+                    </h1>
+                </div>
+            </div>
+
+            <div className='flex gap-[20px] max-w-[1160px] mx-auto mt-[100px]'>
+                <div className='max-w-[360px] max-h-[527px] img-card px-[30px] py-[20px] rounded-[15px]'>
+                    <img className='mt-[-45px]' src="https://images.leadconnectorhq.com/image/f_webp/q_80/r_1200/u_https://assets.cdn.filesafe.space/3cfYZ62ndd3Fci44OcL9/media/6586a959096557121a878ed8.png" alt="" />
+                </div>
+                <div className='max-w-[360px] max-h-[527px] img-card px-[30px] py-[20px] rounded-[15px]'>
+                    <img className='mt-[-45px]' src="https://images.leadconnectorhq.com/image/f_webp/q_80/r_1200/u_https://assets.cdn.filesafe.space/3cfYZ62ndd3Fci44OcL9/media/6587dd4609655731c593bec2.png" alt="" />
+                </div>
+                <div className='max-w-[360px] max-h-[527px] img-card px-[30px] py-[20px] rounded-[15px]'>
+                    <img className='mt-[-45px]' src="https://images.leadconnectorhq.com/image/f_webp/q_80/r_1200/u_https://assets.cdn.filesafe.space/3cfYZ62ndd3Fci44OcL9/media/6586ac0bb3a8812b084ee5d5.png" alt="" />
+                </div>
+            </div>
+
+            <img className='mx-auto mt-[50px]' src='https://images.leadconnectorhq.com/image/f_webp/q_80/r_1200/u_https://assets.cdn.filesafe.space/q6v8KZiyDpfUjDKoCW7u/media/651018c955d7d85166084339.png' alt='' />
+            <img className='mx-auto mt-[50px]' src='https://images.leadconnectorhq.com/image/f_webp/q_80/r_1200/u_https://assets.cdn.filesafe.space/3cfYZ62ndd3Fci44OcL9/media/658be46525e0d6bcc09d24ec.png' alt='' />
+            <img className='mx-auto mt-[20px]' src='https://images.leadconnectorhq.com/image/f_webp/q_80/r_1200/u_https://assets.cdn.filesafe.space/q6v8KZiyDpfUjDKoCW7u/media/651018c9088ee71d6478686d.jpeg' alt='' />
+
+            <div className='flex items-center justify-center py-2'>
+                <div className='bg-[#e73904] w-[732px] h-[111] rounded-3xl p-3 text-white flex items-center  justify-center'>
+                    <h1 className='  font-bold text-[30px] '>BOOK A FREE CONSULTATION CALL <br />
+                        <p className='font-normal text-[20px] text-center'> Schedule call with us</p>
+                    </h1>
+                </div>
+            </div>
+
+            <div className='max-w-[936px] rounded-[15px] mx-auto bg-[#252a2f] px-[30px] py-[20px]'>
+                <p className='text-[#e93d3d] text-[25px] font-bold text-center'>WHY WAIT?</p>
+                <p className='text-white text-[45px] font-bold text-center'>
+                    Discover How To Make $3k-$5k A Month Get More Leads & Sales With High-Converting Funnels 👇
+                </p>
+                <p className='text-white text-[35px] italic font-bold text-center'>
+                    Schedule a Free Consultation Call!
+                </p>
+                <div className='bg-[#e73904] my-[30px] rounded-3xl p-3 text-white flex items-center  justify-center'>
+                    <h1 className='  font-bold text-[30px] '>BOOK A FREE CONSULTATION CALL</h1>
+                </div>
+                <p className='text-white text-[20px] text-center'>Ride the A.I. wave and profit from this rare opportunity!</p>
+            </div>
+
+            <div className='bg-[#f3f4f5] py-[50px] mt-[30px]'>
+                <p className='text-center text-[16px] text-[#555555] mb-[20px]'>Copyright © 2023 - All Rights Reserved.</p>
+                <p className='text-center text-[16px] text-[#555555] max-w-[1130px] mx-auto'>IMPORTANT: This site is not a part of the Facebook website or Facebook Inc. Additionally, this site is NOT endorsed by Facebook in any way. FACEBOOK is a trademark of FACEBOOK, Inc.
+                </p>
+            </div>
+
+        </div>
+    );
+};
+
+export default ThankYouPage;
